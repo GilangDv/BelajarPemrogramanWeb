46a01d921b03cc3980086a30a36df242bf07d4d2
gilangdeva <gilangdevaa@gmail.com>
2020-06-15 14:48:28 +0700

    add file tugas

---
+++ Minggu ke-12/application/views/welcome_message.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">Minggu ke 12</h1>
+		<p class="lead">Gilang Deva Pratama - A12.2018.05955</p>
+	</div>
+</div>
